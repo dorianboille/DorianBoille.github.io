7894caed30c7674ef1d0491ac8fcca2501917af3
dorianboille <dorian.boille63800@gmail.com>
2025-01-11 15:59:31 +0100

    fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/DorianBoille.github.io/', // Remplacez <nom-du-repo> par le nom de votre dépôt GitHub
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
