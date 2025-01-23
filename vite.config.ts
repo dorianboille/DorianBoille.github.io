b5d27d0d0cc32d0da6c71e33ace1ed8c4a8ced33
dorianlrav <dorian.boille@lrav.fr>
2025-01-23 13:17:26 +0100

    test new avec le repo

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio-professionnel/', // Replace with your repository name
+  base: '/DorianBoille.github.io/', // Replace with your repository name
   server: {
     port: 3000,
     host: true
