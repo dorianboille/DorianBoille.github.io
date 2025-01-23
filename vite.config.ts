d7153bacef0192b71ca76d4253ef2b4175ca91b1
dorianlrav <dorian.boille@lrav.fr>
2025-01-23 13:05:11 +0100

    test new

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
+  server: {
+    port: 3000,
+    host: true
   },
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        format: 'es'
+      }
+    }
+  }
 });
